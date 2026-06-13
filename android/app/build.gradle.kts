1f44074ae0826e211f5adb3a95060090173cae47
wildanhiif <whanif999@gmail.com>
2026-06-13 15:45:34 +0700

    fix: Menaikkan compileSdk ke 36 karena limitasi dependensi baru

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.example.utd_advanced_app"
-    compileSdk = 34
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
